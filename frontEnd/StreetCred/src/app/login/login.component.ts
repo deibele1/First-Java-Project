6c2de088125e7e6a16263791c58ac4621344f9ba
Aaron Matthew Deibele <deibele1@gmail.com>
2020-04-07 23:18:43 -0400

    Better GUI and greater functionality

--- frontEnd/StreetCred/src/app/login/login.component.ts
+++ frontEnd/StreetCred/src/app/login/login.component.ts
@@ -8,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  username:string;
+  password:string;
 
   ngOnInit(): void {
+    console.log("Hello Nerds!!!");
+  }
+
+  submit() {
+    console.log(this.username + " " + this.password);
   }
 
 }
