13b58b6bde71d85b297a692bab128ef049e06ff7
John-Aye <eragon1011@hotmail.com>
2020-04-18 16:30:42 -0400

    Signup should now work in theory

---
+++ frontEnd/StreetCred/src/app/Models/UserModel.ts
@@ -0,0 +1,13 @@
+export interface UserModel {
+    username: string;
+    first: string;
+    last: string;
+    email: string;
+    password: string;
+    ans1: string;
+    ans2: string;
+    ans3: string;
+    questionNum1: number;
+    questionNum2: number;
+    questionNum3: number;
+}
--- frontEnd/StreetCred/src/app/Services/app-services.service.ts
+++ frontEnd/StreetCred/src/app/Services/app-services.service.ts
@@ -4,6 +4,7 @@ import { Observable, Subscriber } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { EventModel } from '../Models/EventModel';
 import { Tableable } from '../Models/Tableable';
+import { UserModel } from '../Models/UserModel';
 
 @Injectable()
 export class AppServices {
@@ -28,8 +29,22 @@ export class AppServices {
     return true;
   }
   signup(): boolean {
-    console.log("Method not implemented.");
-    this.http.get(`${AppServices.serviceURL}user/signup`);
+    console.log("Ready for testing");
+
+    let user: UserModel = <UserModel> {
+      username: (<HTMLInputElement>document.getElementById("username")).value,
+      first: (<HTMLInputElement>document.getElementById("first")).value,
+      last: (<HTMLInputElement>document.getElementById("last")).value,
+      email: (<HTMLInputElement>document.getElementById("email")).value,
+      password: (<HTMLInputElement>document.getElementById("ans1")).value,
+      ans1: (<HTMLInputElement>document.getElementById("ans1")).value,
+      ans2: (<HTMLInputElement>document.getElementById("ans2")).value,
+      ans3: (<HTMLInputElement>document.getElementById("ans3")).value,
+      questionNum1: Number.parseInt((<HTMLInputElement>document.getElementById("questionNum1")).value),
+      questionNum2: Number.parseInt((<HTMLInputElement>document.getElementById("questionNum2")).value),
+      questionNum3: Number.parseInt((<HTMLInputElement>document.getElementById("questionNum3")).value)
+    }
+    this.http.post(`${AppServices.serviceURL}user/singup`, user);
     return true;
   }
 }
